3fff51ee02185b174841819ed0a3b627c577f22a
h_watanabe <h.watanabe@dptr.jp>
2023-10-01 05:41:43 +0900

    ポート番号をできるようにする

--- cmd/main.go
+++ cmd/main.go
@@ -4,35 +4,48 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+
 	"golang.org/x/sync/errgroup"
 )
 
 
 func main() {
-	if err := run(context.Background()); err != nil {
-		log.Printf("failed: %v", err)
+	if len(os.Args) != 2 {
+		log.Printf("neet port number")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":3000",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "hello", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
 	}
 
 	eg, ctx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 		err != http.ErrServerClosed {
 			log.Printf("failed to cloade %v", err)
 			return err
 		}
 		return nil
 	})
+
 
 	<-ctx.Done()
 	if err := s.Shutdown(context.Background()); err != nil {
