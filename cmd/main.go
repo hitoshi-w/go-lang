d5154d40d2aa6069e4e38f7449965a67cc0f7c4f
h_watanabe <h.watanabe@dptr.jp>
2023-10-01 01:38:28 +0900

    web サーバーを起動してみる

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+	"os"
 )
 
-func main() {
-	r := gin.Default()
 
-	r.Run()
+func main() {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello, %s", r.URL.Path[1:])
+	})
+
+	err := http.ListenAndServe(":3000", handler)
+
+	if err != nil {
+		fmt.Printf("failed to server: %v", err)
+		os.Exit(1)
+	}
 }
