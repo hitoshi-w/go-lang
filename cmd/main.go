3fee865c0705a748be5c0d769ceab33dc53694d9
h_watanabe <h.watanabe@dptr.jp>
2023-10-01 02:30:45 +0900

    webサーバーのリファクタリング

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+	"golang.org/x/sync/errgroup"
 )
 
 
 func main() {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, %s", r.URL.Path[1:])
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed: %v", err)
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":3000",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "hello", r.URL.Path[1:])
+		}),
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+		err != http.ErrServerClosed {
+			log.Printf("failed to cloade %v", err)
+			return err
+		}
+		return nil
 	})
 
-	err := http.ListenAndServe(":3000", handler)
-
-	if err != nil {
-		fmt.Printf("failed to server: %v", err)
-		os.Exit(1)
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown %v", err)
 	}
+
+	return eg.Wait()
 }
