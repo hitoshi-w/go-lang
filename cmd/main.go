864f33a3087a6a0aca67adb34281f79b022c7e43
h_watanabe <h.watanabe@dptr.jp>
2023-10-02 14:21:25 +0900

    ルーティング定義を分離したNewMuxを定義

--- cmd/main.go
+++ cmd/main.go
@@ -5,14 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/hitoshi-w/go-lang/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -23,9 +18,6 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -37,29 +29,8 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with %v", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-		}),
-	}
+	mux := NewMux()
+	s := NewServer(l, mux)
 
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-		err != http.ErrServerClosed {
-			log.Printf("failed to cloade %v", err)
-			return err
-		}
-		return nil
-	})
-
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown %v", err)
-	}
-
-	return eg.Wait()
+	return s.Run(ctx)
 }
